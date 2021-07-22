5739828d88f1f32fc988b664205858da57ec95c8
favstats <fabio.votta@gmail.com>
2021-07-22 10:08:16 +0200

    separate out R files

--- R/api.R
+++ R/api.R
@@ -180,48 +180,6 @@ wtm_get <- function(endpoint,
 
 
 
-url_form <- function(x, listname) {
-
-    formed_url <- x %>%
-        purrr::map(
-            ~{listname = .x }
-        ) %>%
-        purrr::set_names(rep(listname, length(x)))
-
-
-    return(formed_url)
-}
-
-
-flatten_it <- function(val, nam) {
-    list(val) %>%
-        purrr::flatten() %>%
-        purrr::set_names(nam) %>%
-        purrr::imap(~url_form(.x, .y)) %>%
-        purrr::flatten()
-}
-
-
-wrap_it <- function(arg, arg_names) {
-
-    var_names <- names(arg)
-
-    val <- case_when(
-        arg_names == "values_in_var" ~ "[$in][]",
-        arg_names == "values_nin_var" ~ "[$nin][]",
-        T ~ as.character(glue::glue("[${arg_names}]"))
-    )
-
-
-    vec <- glue::glue("{var_names}{val}")
-
-    in_append <- flatten_it(arg,
-                            vec)
-
-
-    return(in_append)
-
-}
 
 
 # values_in_var <- list(impressions = c(3, 4),
